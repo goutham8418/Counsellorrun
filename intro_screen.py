c34e719091a2a24bf1e9df67064bc1ffce043e72
Blake Wilson <107489039+MyUsernameisAstro@users.noreply.github.com>
2023-07-24 10:08:44 +0100

    test

--- intro_screen.py
+++ intro_screen.py
@@ -5,17 +5,14 @@ import pgzrun
 WIDTH = 600
 HEIGHT = 350
 
-start_button = Actor('startbutton', (108,50))
-
 def draw():
     screen.clear()
-    start_button.draw()
     screen.fill(('White'))
-    screen.blit('titletext',(78, 5))
-    screen.blit('startbutton',(74, 5))
+    #screen.blit('titletext',(78, 5))
+    screen.blit('start',(200, 200))
 
-def on_key_escape():
-        screen.clear()
+#def on_key_escape():
+        #screen.clear()
 
 
 pgzrun.go()
