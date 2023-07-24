a88a0c9e4a7293e41a1ba1eee2df15caf28967ef
Blake Wilson <107489039+MyUsernameisAstro@users.noreply.github.com>
2023-07-24 10:05:25 +0100

    Update intro_screen.py

--- intro_screen.py
+++ intro_screen.py
@@ -5,8 +5,11 @@ import pgzrun
 WIDTH = 600
 HEIGHT = 350
 
+start_button = Actor('startbutton', (108,50))
+
 def draw():
     screen.clear()
+    start_button.draw()
     screen.fill(('White'))
     screen.blit('titletext',(78, 5))
     screen.blit('startbutton',(74, 5))
